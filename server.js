1a7d9ecc57b23ee1c441a6f10c2e49e5d0c05078
Philip Knox <domoncasle@gmail.com>
2019-05-14 19:42:32 -0500

    Node server fix

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 // server.js
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist/StartTimerV2'));
-app.use(bodyParser.json());
-
-// Routes
-app.route('/contact').post((req, res) => {
-    console.log(req.body);
-    res.sendStatus(200);
-});
 
 // Start the app by listening on the default
 // Heroku port
